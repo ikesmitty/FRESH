13dba501c7ffa635e5ec2c92e777478097ae754b
smithi@mail.gvsu.edu <smithi@mail.gvsu.edu>
2018-04-07 13:21:53 -0400

    Changed call to method. Still not successfully hitting the api

--- app/src/main/java/com/example/isaacsmith/fresh/MainActivity.java
+++ app/src/main/java/com/example/isaacsmith/fresh/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.isaacsmith.fresh;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,8 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,22 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try
-        {
-            URL url = new URL("https://www.google.com/");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                readStream(in);
-            } finally {
-                urlConnection.disconnect();
-            }
-        }
-        catch (Exception ex)
-        {
-            Log.d("DEATH", "exception: ", ex);
-            //throw new RuntimeException();
-        }
+        redditAPICall();
     }
 
     private String readStream(InputStream is) {
@@ -54,9 +31,30 @@ public class MainActivity extends AppCompatActivity {
                 bo.write(i);
                 i = is.read();
             }
+            System.out.print("Success: " + bo);
             return bo.toString();
         } catch (IOException e) {
+            System.out.print("FAIL");
             return "";
         }
+    }
+
+    private String redditAPICall(){
+        HttpURLConnection urlConnection = null;
+        try
+        {
+            URL url = new URL("http://www.reddit.com/r/hiphopheads/hot/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } catch(Exception ex){
+
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+
+
+        return "";
     }
 }
