e984f7bbc5e2801a0920dc81ee0ce550add345a9
smithi@mail.gvsu.edu <smithi@mail.gvsu.edu>
2018-04-07 15:09:19 -0400

    Working OAUTH2

--- app/src/main/java/com/example/isaacsmith/fresh/MainActivity.java
+++ app/src/main/java/com/example/isaacsmith/fresh/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.isaacsmith.fresh;
 
 import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.View;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -15,14 +22,110 @@ import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import android.view.SurfaceHolder.Callback;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final String AUTH_URL =
+            "https://www.reddit.com/api/v1/authorize.compact?client_id=%s" +
+            "&response_type=code&state=%s&redirect_uri=%s&" +
+            "duration=permanent&scope=identity";
+
+    private static final String CLIENT_ID = "UzSsz9NRSKYwTw";
+
+    private static final String REDIRECT_URI =
+            "http://www.example.com/my_redirect";
+
+    private static final String STATE = "MY_RANDOM_STRING_1";
+
+    private static final String ACCESS_TOKEN_URL =
+            "https://www.reddit.com/api/v1/access_token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        redditAPICall();
+        //redditAPICall();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        if(getIntent()!=null && getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri uri = getIntent().getData();
+            if(uri.getQueryParameter("error") != null) {
+                String error = uri.getQueryParameter("error");
+                Log.e(TAG, "An error has occurred : " + error);
+            } else {
+                String state = uri.getQueryParameter("state");
+                if(state.equals(STATE)) {
+                    String code = uri.getQueryParameter("code");
+                    getAccessToken(code);
+                }
+            }
+        }
+    }
+
+    private void getAccessToken(String code)
+    {
+        OkHttpClient client = new OkHttpClient();
+        String authString = CLIENT_ID + ":";
+        String encodedAuthString = Base64.encodeToString(authString.getBytes(),
+                Base64.NO_WRAP);
+
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "Sample App")
+                .addHeader("Authorization", "Basic " + encodedAuthString)
+                .url(ACCESS_TOKEN_URL)
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+                        "grant_type=authorization_code&code=" + code +
+                                "&redirect_uri=" + REDIRECT_URI))
+                .build();
+
+        client.newCall(request).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "ERROR: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+
+                JSONObject data = null;
+                try {
+                    data = new JSONObject(json);
+                    String accessToken = data.optString("access_token");
+                    String refreshToken = data.optString("refresh_token");
+
+                    redditAPICall(accessToken);
+
+                    Log.d(TAG, "Access Token = " + accessToken);
+                    Log.d(TAG, "Refresh Token = " + refreshToken);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void SignOn(View view)
+    {
+        String url = String.format(AUTH_URL, CLIENT_ID, STATE, REDIRECT_URI);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 
     private String readStream(InputStream is) {
@@ -41,18 +144,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String redditAPICall(){
+    private String redditAPICall(String accessToken){
         //To Do: Need to setup OAUTH2 for api authentication in app before making this connection call
         HttpURLConnection urlConnection = null;
         try
         {
-            Authenticator.setDefault(new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("ikesmitty", "stephen".toCharArray());
-                }
-            });
-            URL url = new URL("http://www.reddit.com/r/hiphopheads/new.json");
+            URL url = new URL("http://www.reddit.com/r/hiphopheads/new.json" + accessToken);
             urlConnection = (HttpURLConnection) url.openConnection();
             int responseCode = urlConnection.getResponseCode();
             System.out.print("OUR RESPONSE" + responseCode);
